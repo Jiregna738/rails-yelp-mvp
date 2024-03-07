7db9ece89f564071a2fc609424393a64cd32acaa
Jiregna Terfa <jirenlife@gmail.com>
2024-03-07 12:18:51 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :restaurants
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to:"restaurants#create"
+  get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+  get "restaurants/:id/reviews/new", to: "reviews#new", as: "new_review"
+  post "restaurants/:id/reviews", to: "reviews#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.destroy_all
+puts "Destroying all restaurants"
+
+Restaurant.create!(
+  name: "Lalibela",
+  address: "Alexander Platz",
+  phone_number: "1769876534",
+  category: "Chinese"
+)
+Restaurant.create!(
+  name: "Nampan",
+  address: "Charlottenberg",
+  phone_number: "17665376534",
+  category: "japanese"
+)
+Restaurant.create!(
+  name: "Liebe",
+  address: "Hermenplatz",
+  phone_number: "17665376111",
+  category: "italian"
+)
+
+Restaurant.create!(
+  name: "Mumbai",
+  address: "Check-point Charle",
+  phone_number: "17665376111",
+  category: "french"
+)
+
+Restaurant.create!(
+  name: "Olla",
+  address: "Hermenplatz",
+  phone_number: "17665376111",
+  category: "belgian"
+)
+puts "restaurants created"
